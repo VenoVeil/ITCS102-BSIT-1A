77d99e6052d65fc28cafc8c362ae9fe6f09ec462
VenoVeil <vinpoline4@gmail.com>
2026-09-08 10:46:07 +0800

    Add files via upload

---
+++ activity10.py
@@ -0,0 +1,9 @@
+x = 10
+y = 5
+
+
+
+username = 'User1'
+password = 'idkbastagutom'
+
+print(not(x >= y or username == 'user1' and password == 'idkbastagutom'))
---
+++ activity9.py
@@ -0,0 +1,18 @@
+#RELATIONAL OPERATOR DEMO
+x = 5
+y = 10
+user1 = "sample"
+user2 = "sample"
+#1
+print (x > y)
+#2
+print (x < y)
+#3
+print (x <= y)
+#4
+print (x >= y)
+#5
+print (x == y)
+#6
+print (user1 == user2)
+print (user1 != user2)
