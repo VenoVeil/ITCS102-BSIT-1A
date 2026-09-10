2e8edbf117b32dd02c56f7d732c8cff62d6f145a
VenoVeil <vinpoline4@gmail.com>
2026-09-10 08:48:38 +0800

    Add files via upload

---
+++ code_challenge3.py
@@ -0,0 +1,19 @@
+#Input("")
+
+prelim = float(input("Enter Prelim grade: "))
+midterm = float(input("Enter Midterm grade: "))
+semifinal = float(input("Enter Semi-Final grade: "))
+final = float(input("Enter Final grade: "))
+quiz = float(input("Enter Quiz grade: "))
+project = float(input("Enter Project grade: "))
+
+# Computing Final Grade
+
+finalgrade = (prelim * 0.15) + (midterm * 0.15) + (semifinal * 0.15) + (final * 0.15) + (quiz * 0.25) + (project * 0.15)
+
+print("\nFinal Grade:", round(finalgrade, 2))
+
+if finalgrade >= 75:
+    print("Congratulations! You passed the course.")
+else:
+    print("Sorry, you failed.")
